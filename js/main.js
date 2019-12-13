c359a9ad3470d6fe9f6b3a06a4e0f1c381fbbe4c
Kebalepile <pimpabutterfly86@gmail.com>
2019-12-13 06:58:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import note from "./note.js";
+import rec from "./media.js";
+import about from "./about.js";
+
+const recorder = document.querySelector("#recorder"),
+    startRec = document.querySelector(".voice-record"),
+    aboutBtn = document.querySelector("#about");
+
+aboutBtn.onclick = e => {
+    about();
+}
+const display = (recURL) => {
+    notes.appendChild(note(recURL, notes));
+}
+rec(recorder,startRec,display);
